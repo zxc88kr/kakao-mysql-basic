9b575a3c915ee676551fb2de22e89206ec585840
zxc88kr <zxc88kr@naver.com>
2023-06-05 00:07:04 +0900

    feat: Member name change

---
+++ src/test/java/com/example/fastcampusmysql/FastcampusMysqlApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.fastcampusmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusMysqlApplicationTests {
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusMysqlApplicationTests.class, args);
+    }
+}
---
+++ src/test/java/com/example/fastcampusmysql/util/MemberFixtureFactory.java
@@ -0,0 +1,19 @@
+package com.example.fastcampusmysql.util;
+
+import com.example.fastcampusmysql.domain.member.entity.Member;
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+
+import java.util.stream.LongStream;
+
+public class MemberFixtureFactory {
+    public static Member create() {
+        var param = new EasyRandomParameters();
+        return new EasyRandom(param).nextObject(Member.class);
+    }
+
+    public static Member create(Long seed) {
+        var param = new EasyRandomParameters().seed(seed);
+        return new EasyRandom(param).nextObject(Member.class);
+    }
+}
